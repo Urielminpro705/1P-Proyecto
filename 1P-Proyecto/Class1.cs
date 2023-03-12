c02752aaa5c2a7cde6a78b70e2d47e758d3f53da
Uriel Mata Castellanos <urielmata37@hotmail.com>
2023-03-12 12:47:26 -0600

    XD

---
+++ 1P-Proyecto/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1P_Proyecto
+{
+    partial class Class1
+    {
+    }
+}
